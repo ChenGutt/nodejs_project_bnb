5b7c41057c63a00f03f8ab0227230420a28ead14
Chen Gutt <chen.gutt1@gmail.com>
2021-06-05 09:01:54 +0200

    mongoConnectionCheck

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Chen:CG130186@cluster0.22ir7.mongodb.net/bnb?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Chen:Chen123@cluster0.22ir7.mongodb.net/bnb?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
